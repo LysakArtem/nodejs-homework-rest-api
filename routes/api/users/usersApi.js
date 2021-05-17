2b68daf0059984004eeb64fa32424e88b6c69e23
LysakArtem <Perry.dev7@gmail.com>
2021-05-17 20:20:07 +0300

    test

--- routes/api/users/usersApi.js
+++ routes/api/users/usersApi.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+// Test
 const validate = require('./userValidation');
 const userController = require('../../../controllers/usersController');
 const guard = require('../../../helper/quard');
 const uploadAvatar = require('../../../helper/uploadAvatar');
 
-router.post('/registration', validate.Registration, userController.reg);
+router.post(
+  '/registration',
+  validate.Registration,
+  userController.registration
+);
 router.post('/login', validate.Login, userController.login);
 router.post('/logout', guard, userController.logout);
 router.get('/current', guard, userController.current);
